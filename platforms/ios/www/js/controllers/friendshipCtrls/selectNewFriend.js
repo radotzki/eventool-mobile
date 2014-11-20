532e77f8166cb8ee893e4471e3e883fdf7ae1255
Itay Radotzki <radotzki@gmail.com>
2014-11-20 20:10:52 +0200

    Cashier mode

---
+++ platforms/ios/www/js/controllers/friendshipCtrls/selectNewFriend.js
@@ -0,0 +1,35 @@
+angular.module('eventool.controllers')
+
+.controller('SelectNewFriendCtrl', function($scope, $stateParams, $window, Client, Friendship) {
+
+	if(localStorage["clients"]){
+		$scope.clients = JSON.parse(localStorage["clients"]);
+	}
+	else{
+		$scope.loading = true;
+		Client.index().then(function(data){
+			$scope.clients = data;
+			localStorage["clients"] = JSON.stringify(data);
+			$scope.loading = false;
+		});
+	}
+
+	Client.index().then(function(data){
+		localStorage["clients"] = JSON.stringify(data);
+	});
+
+	$scope.doRefresh = function(){
+		Client.index().then(function(data){
+			$scope.clients = data;
+			localStorage["clients"] = JSON.stringify(data);
+			$scope.$broadcast('scroll.refreshComplete');
+		});
+	}
+
+	$scope.choose = function(client){
+		Friendship.create($stateParams.clientId, client.id).then(function(){
+			$window.history.back();
+		})
+	}
+
+ })
