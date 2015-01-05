5c235d2eb7999b2b63ccb815dd90f30b5464af1b
Itay Radotzki <radotzki@gmail.com>
2015-01-06 00:27:33 +0200

    refactor friends index html

--- www/app/friendships/indexFriends.js
+++ www/app/friendships/indexFriends.js
@@ -1,16 +1,28 @@
-(function() {
-	'use strict';
+(function () {
+    'use strict';
 
-	angular
-	.module('eventool.friendship')
-	.controller('IndexFriends', IndexFriends);
+    angular
+        .module('eventool.friendship')
+        .controller('IndexFriends', IndexFriends);
 
-	/* @ngInject */
-	function IndexFriends($stateParams, friendsPrepSvc) {
-		/*jshint validthis: true */
-		var vm = this;
-		vm.clientId = $stateParams.clientId;
-		vm.friends = friendsPrepSvc;
+    /* @ngInject */
+    function IndexFriends($stateParams, friendsPrepSvc) {
+        /*jshint validthis: true */
+        var vm = this;
+        vm.clientId = $stateParams.clientId;
+        vm.friends = friendsPrepSvc;
 
-	}
+        activate();
+
+        function activate() {
+            for (var i = vm.friends.length - 1; i >= 0; i--) {
+                if (vm.friends[i].client_one.id != vm.clientId) {
+                    vm.friends[i].myFriend = vm.friends[i].client_one;
+                } else {
+                    vm.friends[i].myFriend = vm.friends[i].client_two;
+                }
+            };
+        }
+
+    }
 })();
