36b0dae4de122b7bf88e5fcf26732b703d7ccc59
Itay Radotzki <radotzki@gmail.com>
2015-02-24 23:26:34 +0200

    remove client cache

--- www/app/clients/clientsList.js
+++ www/app/clients/clientsList.js
@@ -1,61 +1,56 @@
-(function() {
-	'use strict';
+(function () {
+    'use strict';
 
-	angular
-	.module('eventool.clients')
-	.directive('etClientsList', etClientsList);
+    angular
+        .module('eventool.clients')
+        .directive('etClientsList', etClientsList);
 
-	/* @ngInject */
-	function etClientsList () {
-		var directive = {
-			controller: ClientsList,
-			controllerAs: 'vm',
-			templateUrl: 'app/clients/clientsList.html',
-			restrict: 'EA',
-			bindToController: true,
-			scope: {
-				'clickCallback': '='
-			}
-		};
-		return directive;
-	}
+    /* @ngInject */
+    function etClientsList() {
+        var directive = {
+            controller: ClientsList,
+            controllerAs: 'vm',
+            templateUrl: 'app/clients/clientsList.html',
+            restrict: 'EA',
+            bindToController: true,
+            scope: {
+                'clickCallback': '='
+            }
+        };
+        return directive;
+    }
 
-	/* @ngInject */
-	function ClientsList($scope, $ionicLoading, $localStorage, datacontext, common) {
-		/*jshint validthis: true */
-		var vm = this;
-		
-		vm.clients = [];
-		vm.loading = false;
-		vm.refresh = refresh;
+    /* @ngInject */
+    function ClientsList($scope, $ionicLoading, datacontext, common) {
+        /*jshint validthis: true */
+        var vm = this;
 
-		activate();
-		
+        vm.clients = [];
+        vm.loading = false;
+        vm.refresh = refresh;
 
-		function activate() {
-			if ( $localStorage.clients ) {
-				vm.clients = $localStorage.clients;
-			} else {
-				$ionicLoading.show();
-				getClients().then(function() {
-					$ionicLoading.hide();
-				});
-			}
-		}
+        activate();
 
-		function getClients() {
-			return datacontext.client.index().then(function(data){
-				vm.clients = data;
-				$localStorage.clients = data;
-				return data;
-			});
-		}
 
-		function refresh() {
-			return getClients().then(function() {
-				common.$broadcast('scroll.refreshComplete');
-			});
-		}
+        function activate() {
+            $ionicLoading.show();
+            getClients().then(function () {
+                $ionicLoading.hide();
+            });
+        }
 
-	}
+        function getClients() {
+            return datacontext.client.index().then(function (data) {
+                vm.clients = data;
+                return data;
+            });
+        }
+
+        function refresh() {
+            return getClients().then(function () {
+                common.$broadcast('scroll.refreshComplete');
+            });
+        }
+
+    }
 })();
