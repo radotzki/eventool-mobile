41ad3e26aa094ee7ca8d0afec3089f4fbc23f8d8
Itay Radotzki <radotzki@gmail.com>
2015-06-12 22:22:11 +0300

    delete Recommended Ticket Price feature

--- www/app/tickets/createTicket.js
+++ www/app/tickets/createTicket.js
@@ -10,19 +10,17 @@
         /*jshint validthis: true */
         var vm = this;
 
-        var MIDDLE_AVG = 1.5;
-        var tickets, avgTicketsPrice;
+        var tickets;
         var clientId = $scope.clientId;
 
         vm.createTicket = createTicket;
-        vm.calcBestPrice = calcBestPrice;
         vm.closeModal = $scope.closeModal;
 
         activate();
 
         function activate() {
             getUpcomingEvents();
-            getTickets().then(calcAvgTicketsPrice);
+            getTickets();
         }
 
         function getUpcomingEvents() {
@@ -47,46 +45,6 @@
             datacontext.ticket.create(clientId, param).then(function(res) {
                 vm.closeModal(clientId);
             });
-        }
-
-        function calcBestPrice() {
-        	vm.recommendTicket = null;
-            if (avgTicketsPrice !== -1) {
-                var sortedPrices = _.sortBy(vm.selectedEvent.prices, function(price) {
-                    return price.price;
-                });
-                datacontext.client.freindsTicketsCount(clientId, vm.selectedEvent.id).then(function(resp) {
-                    if (resp.current > resp.average) {
-                        for (var i = 0; i < sortedPrices.length && !vm.recommendTicket; i++) {
-                            if (sortedPrices[i].price > avgTicketsPrice) {
-                                vm.recommendTicket = sortedPrices[i];
-                            }
-                        }
-                    } else {
-                        for (var j = sortedPrices.length - 1; j >= 0 && !vm.recommendTicket; j--) {
-                            if (sortedPrices[j].price < avgTicketsPrice) {
-                                vm.recommendTicket = sortedPrices[j];
-                            }
-                        }
-                    }
-                });
-            }
-        }
-
-        function calcAvgTicketsPrice() {
-            if (tickets && tickets.length) {
-                var avg = _.reduce(tickets, function(sum, ticket) {
-                    return sum + ticket.price.price;
-                }, 0) / tickets.length;
-
-                var middle = _.sortBy(tickets, function(ticket) {
-                    return ticket.price.price;
-                })[Math.floor(tickets.length / 2)].price.price;
-
-                avgTicketsPrice = middle * MIDDLE_AVG >= avg ? middle : avg;
-            } else {
-                avgTicketsPrice = -1;
-            }
         }
 
     }
