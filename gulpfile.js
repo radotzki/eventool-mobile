9b9939c83313a96889a580ea02ecf567a174ba4e
Itay Radotzki <radotzki@gmail.com>
2015-08-11 13:47:43 +0300

    Some clean-up

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,9 @@ require('require-dir')('./gulp');
 
 gulp.task('default', ['sass', 'watch']);
 
-
-// var gutil = require('gulp-util');
-// var bower = require('bower');
-// var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
-// var sh = require('shelljs');
 
 
 gulp.task('sass', function(done) {
@@ -26,23 +21,3 @@ gulp.task('sass', function(done) {
   .pipe(gulp.dest('./www/content/css/'))
   .on('end', done);
 });
-
-// gulp.task('install', ['git-check'], function() {
-//   return bower.commands.install()
-//   .on('log', function(data) {
-//     gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-//   });
-// });
-
-// gulp.task('git-check', function(done) {
-//   if (!sh.which('git')) {
-//     console.log(
-//       '  ' + gutil.colors.red('Git is not installed.'),
-//       '\n  Git, the version control system, is required to download Ionic.',
-//       '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-//       '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-//       );
-//     process.exit(1);
-//   }
-//   done();
-// });
